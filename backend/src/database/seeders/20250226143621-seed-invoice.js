7ed8084d81807afe837714c5bf1b7839a94359de
kkuratolodon <mif05123@gmail.com>
2025-02-26 23:24:50 +0700

    [CHORE] Set data seeding value using faker

--- backend/src/database/seeders/20250226143621-seed-invoice.js
+++ backend/src/database/seeders/20250226143621-seed-invoice.js
@@ -1,25 +1,30 @@
 'use strict';
+const { faker } = require('@faker-js/faker');
 
-/** @type {import('sequelize-cli').Migration} */
 module.exports = {
-  async up (queryInterface, Sequelize) {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
+  async up(queryInterface, Sequelize) {
+    const invoices = [];
+
+    for (let i = 0; i < 10; i++) { // Menambahkan 10 data random
+      invoices.push({
+        invoice_date: faker.date.past(),
+        due_date: faker.date.future(),
+        purchase_order_id: faker.number.int({ min: 100, max: 999 }),
+        total_amount: faker.finance.amount(500, 5000, 2),
+        subtotal_amount: faker.finance.amount(400, 4500, 2),
+        discount_amount: faker.finance.amount(0, 500, 2),
+        payment_terms: faker.helpers.arrayElement(['Net 7', 'Net 10', 'Net 30']),
+        file_url: faker.internet.url(),
+        status: faker.helpers.arrayElement(['pending', 'approved', 'rejected']),
+        createdAt: new Date(),
+        updatedAt: new Date()
+      });
+    }
+
+    await queryInterface.bulkInsert('Invoice', invoices);
   },
 
-  async down (queryInterface, Sequelize) {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
+  async down(queryInterface, Sequelize) {
+    await queryInterface.bulkDelete('Invoice', null, {});
   }
 };
