c5d0956a999aca5641f808ab0bc8145effa4784a
kkuratolodon <mif05123@gmail.com>
2025-02-27 00:18:53 +0700

    [CHORE] Change database config

--- backend/src/database/config.js
+++ backend/src/database/config.js
@@ -8,5 +8,21 @@ module.exports = {
         host: process.env.DB_HOST,
         port: process.env.DB_PORT || 3306,
         dialect: 'mysql'
+    },
+    staging: {
+        username: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME,
+        host: process.env.DB_HOST,
+        port: process.env.DB_PORT || 3306,
+        dialect: 'mysql'
+    },
+    production: {
+        username: process.env.DB_USER_PROD,
+        password: process.env.DB_PASSWORD_PROD,
+        database: process.env.DB_NAME_PROD,
+        host: process.env.DB_HOST_PROD,
+        port: process.env.DB_PORT_PROD || 3306,
+        dialect: 'mysql'
     }
 };
